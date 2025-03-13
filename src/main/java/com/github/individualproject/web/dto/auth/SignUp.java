56824e130bbc70bfcfca1f4a7465fa92211fe3fc
limhhyeon <honghyeon1007@gmail.com>
2025-03-13 15:32:49 +0900

    refactor: 이메일 유효성 검증 && 비밀번호 유효성 검증 추가

--- src/main/java/com/github/individualproject/web/dto/auth/SignUp.java
+++ src/main/java/com/github/individualproject/web/dto/auth/SignUp.java
@@ -2,6 +2,9 @@ package com.github.individualproject.web.dto.auth;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -13,7 +16,14 @@ import java.time.LocalDate;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SignUp {
     private final String name;
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
     private final String email;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+            message = "비밀번호는 8~20자이며, 소문자, 숫자, 특수문자(@$!%*?&)를 각각 1개 이상 포함해야 합니다."
+    )
     private final String password;
     private final String passwordCheck;
     private final LocalDate birthday;
